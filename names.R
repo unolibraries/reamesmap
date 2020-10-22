97c1000286f927eff236238985f2f337d70a91ba
hepplerj <jason.heppler@gmail.com>
2020-10-22 15:17:03 -0500

    Updates to ignore

--- names.R
+++ names.R
@@ -25,7 +25,6 @@ ui <- #bootstrapPage(
                       ),
              ),
              tabPanel("Map",
-                      #tags$style(type = "text/css", "html, body { width: 100%; height: 100%}, .irs-grid-text{color: black, stroke: 2}" ),
                       tags$head(includeCSS("styles.css")),
                       tags$link(rel="stylesheet", type = "text/css", href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"),
                       tags$link(rel="stylesheet", type = "text/css", href = "https://fonts.googleapis.com/css?family=Open+Sans|Source+Sans+Pro"),
